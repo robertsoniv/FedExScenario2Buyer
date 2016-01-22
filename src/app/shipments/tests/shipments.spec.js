ad5d0a2e782f9523a8a60ecc90825d815afcada0
Robert <rwatt@four51.com>
2016-01-22 16:27:56 -0600

    COMP-206 Shipments

--- src/app/shipments/tests/shipments.spec.js
+++ src/app/shipments/tests/shipments.spec.js
@@ -1,10 +1,11 @@
 describe('Component: Shipments', function() {
     var scope,
         q,
-        shipment;
+        shipment,
+        oc;
     beforeEach(module('orderCloud'));
     beforeEach(module('orderCloud.sdk'));
-    beforeEach(inject(function($q, $rootScope) {
+    beforeEach(inject(function($q, $rootScope, OrderCloud) {
         q = $q;
         scope = $rootScope.$new();
         shipment = {
@@ -20,46 +21,47 @@ describe('Component: Shipments', function() {
                 }
             ]
         };
+        oc = OrderCloud;
     }));
 
     describe('State: shipments', function() {
         var state;
-        beforeEach(inject(function($state, Shipments) {
+        beforeEach(inject(function($state) {
             state = $state.get('shipments');
-            spyOn(Shipments, 'List').and.returnValue(null);
+            spyOn(oc.Shipments, 'List').and.returnValue(null);
         }));
-        it('should resolve ShipmentList', inject(function ($injector, Shipments) {
+        it('should resolve ShipmentList', inject(function ($injector) {
             $injector.invoke(state.resolve.ShipmentList);
-            expect(Shipments.List).toHaveBeenCalled();
+            expect(oc.Shipments.List).toHaveBeenCalled();
         }));
     });
 
     describe('State: shipments.edit', function() {
         var state;
-        beforeEach(inject(function($state, Shipments, Orders) {
+        beforeEach(inject(function($state) {
             state = $state.get('shipments.edit');
-            spyOn(Shipments, 'Get').and.returnValue(null);
-            spyOn(Orders, 'List').and.returnValue(null);
+            spyOn(oc.Shipments, 'Get').and.returnValue(null);
+            spyOn(oc.Orders, 'List').and.returnValue(null);
         }));
-        it('should resolve SelectedShipment', inject(function ($injector, $stateParams, Shipments) {
+        it('should resolve SelectedShipment', inject(function ($injector, $stateParams) {
             $injector.invoke(state.resolve.SelectedShipment);
-            expect(Shipments.Get).toHaveBeenCalledWith($stateParams.shipmentid);
+            expect(oc.Shipments.Get).toHaveBeenCalledWith($stateParams.shipmentid);
         }));
-        it('should resolve OrderList', inject(function ($injector, $stateParams, Orders) {
+        it('should resolve OrderList', inject(function ($injector) {
             $injector.invoke(state.resolve.OrderList);
-            expect(Orders.List).toHaveBeenCalledWith('incoming');
+            expect(oc.Orders.List).toHaveBeenCalledWith('incoming');
         }));
     });
 
     describe('State: shipments.create', function() {
         var state;
-        beforeEach(inject(function($state, Orders) {
+        beforeEach(inject(function($state) {
             state = $state.get('shipments.create');
-            spyOn(Orders, 'List').and.returnValue(null);
+            spyOn(oc.Orders, 'List').and.returnValue(null);
         }));
-        it('should resolve OrderList', inject(function ($injector, $stateParams, Orders) {
+        it('should resolve OrderList', inject(function ($injector) {
             $injector.invoke(state.resolve.OrderList);
-            expect(Orders.List).toHaveBeenCalledWith('incoming');
+            expect(oc.Orders.List).toHaveBeenCalledWith('incoming');
         }));
     });
 
@@ -100,8 +102,8 @@ describe('Component: Shipments', function() {
         });
 
         describe('deleteLineItem', function() {
-            beforeEach(inject(function(Shipments) {
-                spyOn(Shipments, 'Patch').and.returnValue(null);
+            beforeEach(function() {
+                spyOn(oc.Shipments, 'Patch').and.returnValue(null);
                 shipmentEditCtrl.OrderSelected = false;
                 shipmentEditCtrl.lineitems.list.Items = [
                             {
@@ -112,10 +114,10 @@ describe('Component: Shipments', function() {
                 var index = 0;
                 shipmentEditCtrl.shipment = shipment;
                 shipmentEditCtrl.deleteLineItem(index);
-            }));
-            it('should call the Shipments Patch method', inject(function (Shipments) {
-                expect(Shipments.Patch).toHaveBeenCalledWith(shipment.ID, {Items: shipmentEditCtrl.shipment.Items});
-            }));
+            });
+            it('should call the Shipments Patch method', function () {
+                expect(oc.Shipments.Patch).toHaveBeenCalledWith(shipment.ID, {Items: shipmentEditCtrl.shipment.Items});
+            });
             it('should make addToShipment false', inject(function () {
                 expect(shipmentEditCtrl.lineitems.list.Items[0].addToShipment).toEqual(false);
             }));
@@ -128,70 +130,70 @@ describe('Component: Shipments', function() {
         });
 
         describe('goToLineItems', function() {
-            beforeEach(inject(function(LineItems) {
+            beforeEach(function() {
                 order = {
                     ID: 'TestOrder123456789'
-                }
+                };
                 var defer = q.defer();
                 defer.resolve(shipment);
-                spyOn(LineItems, 'List').and.returnValue(defer.promise);
+                spyOn(oc.LineItems, 'List').and.returnValue(defer.promise);
                 shipmentEditCtrl.goToLineItems(order);
                 scope.$digest();
-            }));
-            it('should call the LineItems List method', inject(function (LineItems) {
-                expect(LineItems.List).toHaveBeenCalledWith(order.ID, 1, 20);
-            }));
+            });
+            it('should call the LineItems List method', function () {
+                expect(oc.LineItems.List).toHaveBeenCalledWith(order.ID, 1, 20);
+            });
         });
 
         describe('Submit', function() {
-            beforeEach(inject(function(Shipments) {
+            beforeEach(function() {
                 shipmentEditCtrl.shipment = shipment;
                 shipmentEditCtrl.shipmentID = "TestShipment123456789";
                 var defer = q.defer();
                 defer.resolve(shipment);
-                spyOn(Shipments, 'Update').and.returnValue(defer.promise);
+                spyOn(oc.Shipments, 'Update').and.returnValue(defer.promise);
                 shipmentEditCtrl.Submit();
                 scope.$digest();
-            }));
-            it ('should call the Shipments Update method', inject(function(Shipments) {
-                expect(Shipments.Update).toHaveBeenCalledWith(shipmentEditCtrl.shipmentID, shipmentEditCtrl.shipment);
-            }));
+            });
+            it ('should call the Shipments Update method', function() {
+                expect(oc.Shipments.Update).toHaveBeenCalledWith(shipmentEditCtrl.shipmentID, shipmentEditCtrl.shipment);
+            });
             it ('should enter the shipments state', inject(function($state) {
                 expect($state.go).toHaveBeenCalledWith('shipments', {}, {reload:true});
             }));
         });
 
         describe('Delete', function() {
-            beforeEach(inject(function(Shipments) {
+            beforeEach(function() {
                 var defer = q.defer();
                 defer.resolve(shipment);
-                spyOn(Shipments, 'Delete').and.returnValue(defer.promise);
+                spyOn(oc.Shipments, 'Delete').and.returnValue(defer.promise);
                 shipmentEditCtrl.Delete();
                 scope.$digest();
-            }));
-            it ('should call the Shipments Delete method', inject(function(Shipments) {
-                expect(Shipments.Delete).toHaveBeenCalledWith(shipment.ID, false);
-            }));
+            });
+            it ('should call the Shipments Delete method', function() {
+                expect(oc.Shipments.Delete).toHaveBeenCalledWith(shipment.ID, false);
+            });
             it ('should enter the shipments state', inject(function($state) {
                 expect($state.go).toHaveBeenCalledWith('shipments', {}, {reload:true});
             }));
         });
 
         describe('pagingfunction', function() {
-            beforeEach(inject(function(LineItems) {
-                spyOn(LineItems, 'List').and.returnValue(null);
+            beforeEach(function() {
+                spyOn(oc.LineItems, 'List').and.returnValue(null);
                 shipmentEditCtrl.lineitems.list = {
                     Meta: {
                         Page: 1,
                         TotalPages: 2,
                         PageSize: 20
                     }
-                }
+                };
                 shipmentEditCtrl.lineitems.pagingfunction();
-            }));
-            it ('should call the LineItems List method', inject(function(LineItems) {
-                expect(LineItems.List).toHaveBeenCalledWith(false, shipmentEditCtrl.lineitems.list.Meta.Page +1, shipmentEditCtrl.lineitems.list.Meta.PageSize);
-            }));
+            });
+            it ('should call the LineItems List method', function() {
+                expect(oc.LineItems.List).toHaveBeenCalledWith(false, shipmentEditCtrl.lineitems.list.Meta.Page +1, shipmentEditCtrl.lineitems.list.Meta.PageSize);
+            });
         });
     });
 
@@ -206,19 +208,19 @@ describe('Component: Shipments', function() {
         }));
 
         describe('goToLineItems', function() {
-            beforeEach(inject(function(LineItems) {
+            beforeEach(function() {
                 order = {
                     ID: 'TestOrder123456789'
-                }
+                };
                 var defer = q.defer();
                 defer.resolve(shipment);
-                spyOn(LineItems, 'List').and.returnValue(defer.promise);
+                spyOn(oc.LineItems, 'List').and.returnValue(defer.promise);
                 shipmentCreateCtrl.goToLineItems(order);
                 scope.$digest();
-            }));
-            it('should call the LineItems List method', inject(function (LineItems) {
-                expect(LineItems.List).toHaveBeenCalledWith(order.ID, 1, 20);
-            }));
+            });
+            it('should call the LineItems List method', function () {
+                expect(oc.LineItems.List).toHaveBeenCalledWith(order.ID, 1, 20);
+            });
         });
 
         describe('unselectOrder', function() {
@@ -236,17 +238,17 @@ describe('Component: Shipments', function() {
         });
 
         describe('Submit', function() {
-            beforeEach(inject(function(Shipments) {
+            beforeEach(function() {
                 shipmentCreateCtrl.shipment = shipment;
                 var defer = q.defer();
                 defer.resolve(shipment);
-                spyOn(Shipments, 'Create').and.returnValue(defer.promise);
+                spyOn(oc.Shipments, 'Create').and.returnValue(defer.promise);
                 shipmentCreateCtrl.Submit();
                 scope.$digest();
-            }));
-            it ('should call the Shipments Create method', inject(function(Shipments) {
-                expect(Shipments.Create).toHaveBeenCalledWith(shipment);
-            }));
+            });
+            it ('should call the Shipments Create method', function() {
+                expect(oc.Shipments.Create).toHaveBeenCalledWith(shipment);
+            });
             it ('should enter the shipments state', inject(function($state) {
                 expect($state.go).toHaveBeenCalledWith('shipments', {}, {reload:true});
             }));
